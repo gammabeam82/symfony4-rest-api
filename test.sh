a9cfef30497920d45a3a9449b41f9cd65471c15b
Paul Mastavy <gammabeam82@gmail.com>
2018-11-18 19:59:07 +0300

    Update bash scripts.

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-php bin/console doctrine:database:drop --env=test --no-debug --force --if-exists
-php bin/console doctrine:database:create --env=test --no-debug --no-interaction --if-not-exists
-php bin/console doctrine:migrations:migrate --env=test --no-debug --no-interaction --quiet
-php bin/console doctrine:fixtures:load --env=test --no-debug --no-interaction
-php bin/console cache:clear --env=test --no-debug --no-warmup
+CONSOLE="php bin/console"
+
+$CONSOLE doctrine:database:drop --env=test --no-debug --force --if-exists
+$CONSOLE doctrine:database:create --env=test --no-debug --no-interaction --if-not-exists
+$CONSOLE doctrine:migrations:migrate --env=test --no-debug --no-interaction --quiet
+$CONSOLE doctrine:fixtures:load --env=test --no-debug --no-interaction
+$CONSOLE cache:clear --env=test --no-debug --no-warmup
 
 export SYMFONY_DEPRECATIONS_HELPER=weak
 
